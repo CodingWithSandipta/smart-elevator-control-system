3daece352781d9719080f359d96caf18481b0628
Sandip Bhattacharyya <sandiptabhattacharyya2345@gmail.com>
2026-03-16 00:26:48 +0530

    Implemented EmergencyState for elevator safety control & Implemented ElevatorController to manage requests and state transitions

--- controller/ElevatorController.java
+++ controller/ElevatorController.java
@@ -1,5 +1,54 @@
 package controller;
 
+import model.Elevator;
+import model.Request;
+import state.MovingDownState;
+import state.MovingUpState;
+
 public class ElevatorController {
-    
+
+    private Elevator elevator;
+
+    public ElevatorController(Elevator elevator) {
+        this.elevator = elevator;
+    }
+
+    public void submitRequest(Request request) {
+
+        System.out.println("New request received for floor " + request.getFloorNumber());
+
+        elevator.getCurrentState().handleRequest(elevator, request);
+
+        decideDirection();
+    }
+
+    private void decideDirection() {
+
+        if (!elevator.getPriorityRequests().isEmpty()) {
+
+            int target = elevator.getPriorityRequests().peek().getFloorNumber();
+
+            if (target > elevator.getCurrentFloor()) {
+                elevator.setCurrentState(new MovingUpState());
+            } else {
+                elevator.setCurrentState(new MovingDownState());
+            }
+
+        } 
+        else if (!elevator.getNormalRequests().isEmpty()) {
+
+            int target = elevator.getNormalRequests().peek().getFloorNumber();
+
+            if (target > elevator.getCurrentFloor()) {
+                elevator.setCurrentState(new MovingUpState());
+            } else {
+                elevator.setCurrentState(new MovingDownState());
+            }
+
+        }
+    }
+
+    public void runElevator() {
+        elevator.getCurrentState().move(elevator);
+    }
 }
